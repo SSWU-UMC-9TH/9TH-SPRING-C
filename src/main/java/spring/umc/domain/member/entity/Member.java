4b8c318e2d607114745e4a2f5006eea4bdefbe7c
yjhss <20211420@sungshin.ac.kr>
2025-10-02 16:51:07 +0900

    [FEAT] Member 엔티티 추가

---
+++ src/main/java/spring/umc/domain/member/entity/Member.java
@@ -0,0 +1,55 @@
+package spring.umc.domain.member.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import spring.umc.domain.member.enums.Gender;
+import spring.umc.domain.member.enums.SocialType;
+import spring.umc.global.entity.BaseEntity;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Table(name = "member")
+public class Member extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", length = 10, nullable = false)
+    private String name;
+
+    @Column(name = "gender", nullable = false)
+    @Enumerated(EnumType.STRING)
+    @Builder.Default
+    private Gender gender = Gender.NONE;
+
+    @Column(name = "birth_date", nullable = false)
+    private LocalDate birthDate;
+
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    @Column(name = "social_uid")
+    private String socialUid;
+
+    @Column(name = "social_type")
+    @Enumerated(EnumType.STRING)
+    private SocialType socialType;
+
+    @Column(name = "points")
+    private int points;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone_number", length = 20)
+    private String phoneNumber;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+}
---
+++ src/main/java/spring/umc/domain/member/enums/FoodName.java
@@ -0,0 +1,6 @@
+package spring.umc.domain.member.enums;
+
+public enum FoodName {
+    KOREAN, JAPANESE, CHINESE, WESTERN, CHICKEN, SNACK,
+    MEAT, LUNCH, NIGHT, FASTFOOD, DESSERT, ASIAN
+}

f4ac6f929a636321cd08af43053ffa7ad323ba4c
yjhss <20211420@sungshin.ac.kr>
2025-10-03 10:51:12 +0900

    [FEAT] Mission, MemberMission 엔티티 추가

---
+++ src/main/java/spring/umc/domain/mission/entity/mapping/MemberMission.java
@@ -0,0 +1,31 @@
+package spring.umc.domain.mission.entity.mapping;
+
+import jakarta.persistence.*;
+import lombok.*;
+import spring.umc.domain.member.entity.Member;
+import spring.umc.domain.mission.entity.Mission;
+import spring.umc.global.entity.BaseEntity;
+
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Table(name = "member_mission")
+public class MemberMission extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "is_completed", nullable = false)
+    private boolean isCompleted;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mission_id")
+    private Mission mission;
+}
